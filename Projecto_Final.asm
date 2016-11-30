13d46bb1b0a9e68cb95a5759a881c8a02e2e10b7
DESKTOP-755LG57\Preto <odirbecas@iol.pt>
2016-11-30 11:41:32 +0000

    no message

--- Projecto_Final.asm
+++ Projecto_Final.asm
@@ -1020,6 +1020,7 @@ fim_desenhar_pixel:
     POP R1 ; Devolve R1
     POP R0 ; Devolve R0
     RET ; Termina rotina
+	
 ; **********************************************************************
 ; Escreve Ecrã
 ;   Rotina que desenha uma tabela de strings no ecrã
@@ -1269,13 +1270,51 @@ descer_tetra:
 ; Pausa
 ;   Rotina que faz uma pausa.
 ; Entradas:
-;    - 
+;   Nenhuma
 ; Saidas:
 ;   R10 - 1:se pode desenhar 0:se n pode
 ; **********************************************************************
 
 verificar_pixel:
-
+    PUSH  R0 ; Guarda R0
+    PUSH  R1 ; Guarda R1
+    PUSH  R2 ; Guarda R2
+    PUSH  R3 ; Guarda R3
+    PUSH  R4 ; Guarda R4
+    PUSH  R5 ; Guarda R5
+    PUSH  R6 ; Guarda R6
+    PUSH  R7 ; Guarda R7
+    PUSH  R8 ; Guarda R8
+	MOV R0, local_Ecra ; Atualiza R0 com o endereco do ecra
+	MOV R4, 4 ; Atualiza R4 com o numero de bytes de cada linha do ecra (linhas)
+	MOV R5, 8 ; Atualiza R5 com o numero de bits por byte do ecra (colunas)
+	MOV R6, 80H ; (10000000) Mascara que permite escrever nas colunas de cada byte atraves de shifts
+	MUL R4, R2 ; Transforma R2 numa linha do ecra
+	ADD R0, R4 ; Acede a linha do ecra onde e suposto escrever
+	MOV R7, R3 ; R7 com o valor da coluna para dividir pelo numero de colunas
+	DIV R7, R5 ; Transforma R7 numa coluna da linha onde escrever
+	ADD R0, R7 ; Acede a coluna onde escrever
+	MOD R3, R5 ; R3 com o bit onde escrever (R3 sera um valor de 0-7 que sao o numero de colunas)
+selecao_pixel1:
+	AND R3, R3 ; Afectar as flags
+	JZ verif_pintado ; Quando for o ultimo bit a verificar salta para decidir se desenha ou apaga
+	SHR R6, 1 ; Atualiza a mascara para o bit seguinte ate ao bit onde e suposto escrever
+	SUB R3, 1 ; Atualiza a coluna (avanca para o proximo bit)
+	JMP selecao_pixel ; Verifica os bits todos de R3
+verif_pintado:
+	MOVB R8, [R0] ; MOVe para R8 o que esta escrito no byte do ecra
+	AND R8, R6 ; Apaga apenas o bit escolhido
+fim_verificar_pixel:
+    POP R8 ; Devolve R8
+    POP R7 ; Devolve R7
+    POP R6 ; Devolve R6
+    POP R5 ; Devolve R5
+    POP R4 ; Devolve R4
+    POP R3 ; Devolve R3
+    POP R2 ; Devolve R2
+    POP R1 ; Devolve R1
+    POP R0 ; Devolve R0
+    RET ; Termina rotina
 	
 ; **********************************************************************
 ; Interrupçao 0
